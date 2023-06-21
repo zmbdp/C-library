d0de57a4f022de4b0453d1bab528b70398e991df
稚名不带撇 <118424032+Bitzmbdp@users.noreply.github.com>
2023-06-21 08:15:27 +0800

    伪代码

---
+++ Project1/Test.c
@@ -0,0 +1,10 @@
+#pragma warning(disable:4996)
+#include<stdio.h> 
+int main() 
+{
+	SLT* node = (SLT*)malloc(sizeof(SLT));
+	node->data = data;
+	node->next = head->next;
+	head->next = node;
+	return 0;
+}
